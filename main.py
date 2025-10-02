57b52020398ce74bdc94fc673863fabad2542ce2
Barmaid2531 <tintobabu@gmail.com>
2025-10-02 15:34:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+# Import the function from your dashboard file
+from ui.dashboard import run_app
+
+# Run the app
+if __name__ == "__main__":
+    run_app()
 print("Run the dashboard with:")
 print("streamlit run ui/dashboard.py")
